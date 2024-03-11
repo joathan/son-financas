cbe625150d4f55ba319dbf00090e7eff5de4a388
Joathan Souza <joathanf@gmail.com>
2024-03-11 01:26:48 -0300

    configurando MySQL na aplicação

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'son_financas',
+        'username' => 'financas',
+        'password' => 'financas',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ],
+];
