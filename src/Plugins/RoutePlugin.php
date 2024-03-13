92e63a484bc83ecfce7173d607323f73aa45f09b
Joathan Souza <joathanf@gmail.com>
2024-03-13 00:01:23 -0300

    configurando combinador de rotas

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,9 +1,12 @@
 <?php
-
+declare(strict_types=1);
 namespace SONFin\Plugins;
 
 use Aura\Router\RouterContainer;
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
 use SONFin\ServiceContainerInterface;
+use Zend\Diactoros\ServerRequest;
 
 class RoutePlugin implements PluginInterface
 {
@@ -13,9 +16,24 @@ class RoutePlugin implements PluginInterface
         $map = $routerContainer->getMap();
         $matcher = $routerContainer->getMatcher();
         $generator = $routerContainer->getGenerator();
+        $request = $this->getRequest();
 
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
-    } 
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+            return $matcher->match($request);
+        });
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
+    }
 }
