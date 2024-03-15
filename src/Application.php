59a9d623d62a7f30bf3fe93e3c242154fcf761fb
Joathan Souza <joathanf@gmail.com>
2024-03-15 01:01:27 -0300

    corrigindo espaçamento

--- src/Application.php
+++ src/Application.php
@@ -18,52 +18,52 @@ class Application
 
     public function service($name)
     {
-      return $this->serviceContainer->get($name);
+        return $this->serviceContainer->get($name);
     }
-    
+
     public function addService(string $name, $service): void
     {
-      if (is_callable($service)) {
-        $this->serviceContainer->addLazy($name, $service);
-      } else {
-        $this->serviceContainer->add($name, $service);
-      }
+        if (is_callable($service)) {
+            $this->serviceContainer->addLazy($name, $service);
+        } else {
+            $this->serviceContainer->add($name, $service);
+        }
     }
 
     public function plugin(PluginInterface $plugin): void
     {
-      $plugin->register($this->serviceContainer);
+        $plugin->register($this->serviceContainer);
     }
 
     public function get($path, $action, $name = null): Application
     {
-      $routing = $this->service('routing');
-      $routing->get($name, $path, $action);
-      return $this;
+        $routing = $this->service('routing');
+        $routing->get($name, $path, $action);
+        return $this;
     }
 
     public function start()
     {
-      $route = $this->service('route');
-      $request = $this->service(RequestInterface::class);
+        $route = $this->service('route');
+        $request = $this->service(RequestInterface::class);
 
-      if (!$route) {
-        echo "Page not found";
-        exit;
-      }
+        if (!$route) {
+            echo "Page not found";
+            exit;
+        }
 
-      foreach ($route->attributes as $key => $value) {
-        $request = $request->withAttribute($key, $value);
-      }
+        foreach ($route->attributes as $key => $value) {
+            $request = $request->withAttribute($key, $value);
+        }
 
-      $callable = $route->handler;
-      $response = $callable($request);
-      $this->emiResponse($response);
+        $callable = $route->handler;
+        $response = $callable($request);
+        $this->emiResponse($response);
     }
 
     protected function emiResponse(ResponseInterface $response)
     {
-      $emitter = new SapiEmitter();
-      $emitter->emit($response);
+        $emitter = new SapiEmitter();
+        $emitter->emit($response);
     }
 }
