9d514cd634f4fa0509389c89e968e7375e4af74d
Joathan Souza <joathanf@gmail.com>
2024-03-15 23:39:41 -0300

    criando o plugin DbPlugin para conexão com banco de dados

---
+++ src/Plugins/DbPlugin.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+namespace SONFin\Plugins;
+
+use SONFin\ServiceContainerInterface;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class DbPlugin implements PluginInterface
+{
+    public function register(ServiceContainerInterface $container)
+    {
+        $capsule = new Capsule;
+        $config = include __DIR__ . '/../../config/db.php';
+        $capsule->addConnection($config);
+        $capsule->addConnection($config['development']);
+        $capsule->bootEloquent();        
+    }
+}
