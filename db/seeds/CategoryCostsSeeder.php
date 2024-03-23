74c0c682a0e8563daa70907683e8c08cb7bc2061
Joathan Souza <joathanf@gmail.com>
2024-03-23 15:28:35 -0300

    associando categoria ao usuario

--- db/seeds/CategoryCostsSeeder.php
+++ db/seeds/CategoryCostsSeeder.php
@@ -4,6 +4,22 @@ use Phinx\Seed\AbstractSeed;
 
 class CategoryCostsSeeder extends AbstractSeed
 {
+    const NAMES = [
+        'Telefone',
+        'Água',
+        'Luz',
+        'Escola',
+        'Cartão',
+        'Gasolina',
+        'Vestuário',
+        'Alimentação',
+        'Lazer',
+        'Supermercado',
+        'IPVA',
+        'Entretenimento',
+        'Imposto de Renda',
+        'Outros'
+    ];
     /**
      * Run Method.
      *
@@ -15,17 +31,24 @@ class CategoryCostsSeeder extends AbstractSeed
     public function run()
     {
         $faker = \Faker\Factory::create('pt_BR');
+        $faker->addProvider($this);
         $categoryCosts = $this->table('category_costs');
         $data = [];
 
-        foreach (range(1, 10) as $value) {
+        foreach (range(1, 20) as $value) {
             $data[] = [
-                'name' => $faker->name,
+                'name' => $faker->categoryName(),
+                'user_id' => rand(1, 4), 
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ];
         }
 
         $categoryCosts->insert($data)->save();
+    }
+
+    public function categoryName()
+    {
+        return \Faker\Provider\Base::randomElement(self::NAMES);
     }
 }
--- src/Models/CategoryCost.php
+++ src/Models/CategoryCost.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryCost extends Model
 {
     protected $fillable = [
-        'name'
+        'name',
+        'user_id',
     ];
 
 }
